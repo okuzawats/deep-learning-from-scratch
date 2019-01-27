a7d36f633dde0c48e947a4b0f822a7f10a535d1a
okuzawats <okuzawats@gmail.com>
2019-01-27 15:27:24 +0900

    NANDの実装

--- chapter-2/chapter-2.py
+++ chapter-2/chapter-2.py
@@ -10,8 +10,23 @@ def AND(x1, x2):
 	else:
 		return 1
 
+def NAND(x1, x2):
+	x = np.array([x1, x2])
+	w = np.array([-0.5, -0.5])
+	b = 0.7
+	tmp = np.sum(x * w) + b
+	if tmp <= 0:
+		return 0
+	else:
+		return 1
+
 if __name__ == '__main__':
-	print(AND(0, 0))
-	print(AND(1, 0))
-	print(AND(0, 1))
-	print(AND(1, 1))
+	print(AND(0, 0)) #0
+	print(AND(1, 0)) #0
+	print(AND(0, 1)) #0
+	print(AND(1, 1)) #1
+
+	print(NAND(0, 0)) #1
+	print(NAND(1, 0)) #1
+	print(NAND(0, 1)) #1
+	print(NAND(1, 1)) #0
