d2e23eeb497605ea885f8439990388b7f0c4991a
okuzawats <okuzawats@gmail.com>
2019-01-27 15:29:26 +0900

    ORの実装

--- chapter-2/chapter-2.py
+++ chapter-2/chapter-2.py
@@ -20,13 +20,31 @@ def NAND(x1, x2):
 	else:
 		return 1
 
+def OR(x1, x2):
+	x = np.array([x1, x2])
+	w = np.array([0.5, 0.5])
+	b = -0.2
+	tmp = np.sum(x * w) + b
+	if tmp <= 0:
+		return 0
+	else:
+		return 1
+
 if __name__ == '__main__':
+	# AND
 	print(AND(0, 0)) #0
 	print(AND(1, 0)) #0
 	print(AND(0, 1)) #0
 	print(AND(1, 1)) #1
 
+	# NAND
 	print(NAND(0, 0)) #1
 	print(NAND(1, 0)) #1
 	print(NAND(0, 1)) #1
 	print(NAND(1, 1)) #0
+
+	# OR
+	print(OR(0, 0)) #0
+	print(OR(1, 0)) #1
+	print(OR(0, 1)) #1
+	print(OR(1, 1)) #1
