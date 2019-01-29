225f82e9505c562ea774f8120998eaf0f5b5a84e
okuzawats <okuzawats@gmail.com>
2019-01-29 23:11:03 +0900

    シグモイド関数の実装と表示

--- deep-learning-from-scratch.py
+++ deep-learning-from-scratch.py
@@ -41,9 +41,12 @@ def step_function(x):
 	y = x > 0
 	return y.astype(np.int)
 
+def sigmoid(x):
+	return 1 / (1 + np.exp(-x))
+
 if __name__ == '__main__':
 	x = np.arange(-5.0, 5.0, 0.1)
-	y = step_function(x)
+	y = sigmoid(x)
 	plt.plot(x, y)
 	plt.ylim(-0.1, 1.1)
 	plt.show()
