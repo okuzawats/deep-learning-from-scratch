f8f22add4f0c2afdbe3cec1479b09d3931e49b31
okuzawats <okuzawats@gmail.com>
2019-01-30 22:19:48 +0900

    softmax関数の修正

--- deep-learning-from-scratch.py
+++ deep-learning-from-scratch.py
@@ -45,7 +45,8 @@ def sigmoid(x):
 	return 1 / (1 + np.exp(-x))
 
 def softmax(a):
-	exp_a = np.exp(a)
+	c = np.max(a)
+	exp_a = np.exp(a - c) # オーバーフロー対策
 	sum_exp_a = np.sum(exp_a)
 	y = exp_a / sum_exp_a
 
