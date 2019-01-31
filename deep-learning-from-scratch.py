14906785ee1b5d286fa7a33161b84c8214153782
okuzawats <okuzawats@gmail.com>
2019-02-01 08:42:33 +0900

    関数の順序を変更

--- deep-learning-from-scratch.py
+++ deep-learning-from-scratch.py
@@ -47,6 +47,9 @@ def sigmoid(x):
 def relu(x):
 	return np.maximum(0, x)
 
+def identity_function(x):
+	return x
+
 def softmax(a):
 	c = np.max(a)
 	exp_a = np.exp(a - c) # オーバーフロー対策
@@ -54,9 +57,6 @@ def softmax(a):
 	y = exp_a / sum_exp_a
 
 	return y
-
-def identity_function(x):
-	return x
 
 def init_network():
 	network = {}
