ae3081c3df7ed65b26feebec5cea2b4219fd5b27
okuzawats <okuzawats@gmail.com>
2019-01-31 22:03:35 +0900

    ReLU関数の実装

--- deep-learning-from-scratch.py
+++ deep-learning-from-scratch.py
@@ -44,6 +44,9 @@ def step_function(x):
 def sigmoid(x):
 	return 1 / (1 + np.exp(-x))
 
+def relu(x):
+	return np.maximum(0, x)
+
 def softmax(a):
 	c = np.max(a)
 	exp_a = np.exp(a - c) # オーバーフロー対策
